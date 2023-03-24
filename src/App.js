b13d30c927e6ba111b1ffdf074a06cf1cc0438c0
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-03-24 13:08:47 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
+
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
+
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />}/>
+          <Route path='/company' element={<Company />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/newproject' element={<NewProject />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/project/:id' element={<Project />}/>
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
